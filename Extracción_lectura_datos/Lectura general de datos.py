dbf0759daf223af773e80cb084995aa393df8d84
jorgeduque97 <jorgeduque97@hotmail.com>
2021-03-31 13:48:34 +0200

    Creación de distintas funciones para extraer los datos necesarios para el proyecto

---
+++ Extracción_lectura_datos/Lectura general de datos.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import requests
+import pandas_datareader as pdr
+from bs4 import BeautifulSoup
+import datetime
+
+
+def fill_missing_dates(df):
+    df = df[~df.index.duplicated()]
+    idx = pd.date_range(df.index[0].strftime("%d-%m-%y"), df.index[-1].strftime("%d-%m-%y"))
+    df = df.reindex(idx)
+    df.fillna(method='ffill', inplace=True)
+    return df
+
+
+def extract_data_from_interactive_chart(url, content):
+    session = requests.Session()
+    page = session.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    data = str(soup.find_all('script')[4])
+    data = data.split('d = new Dygraph(document.getElementById("container"),')[1].split(', {labels: ')[0]
+    dates = []
+    values = []
+
+    for i in range(data.count('new Date')):
+        date = data.split('new Date("')[i + 1].split('"')[0]
+        value = data.split('"),')[i + 1].split(']')[0]
+        dates.append(date)
+        values.append(value)
+
+    df = pd.DataFrame(list(zip(dates, values)), columns=["Date", content])
+    df['Date'] = pd.to_datetime(df['Date'])
+    df.set_index(['Date'], inplace=True)
+    return df
+
+
+def extract_data_from_yahoo_finance(description, symbol):
+    start = datetime.datetime(2011, 1, 1)
+    end = datetime.datetime(2020, 12, 31)
+    if symbol == 'BTC-USD':
+        df = pdr.get_data_yahoo(symbol, start=start, end=end).loc[:, ['Volume', 'Adj Close']]
+        df.rename(columns={'Adj Close': description, 'Volume': 'Bitcoin_Volumen'}, inplace=True)
+        df = fill_missing_dates(df)
+        return df
+    else:
+        df = pdr.get_data_yahoo(symbol, start=start, end=end).loc[:, 'Adj Close'].to_frame()
+        df.rename(columns={'Adj Close': description}, inplace=True)
+        df = fill_missing_dates(df)
+        return df
+
+
+def main():
+    frames = []
+    with open('URLs.txt', 'r') as f:
+        elements = [line.rstrip('\n').split(',') for line in f]
+    for url, content in elements:
+        frames.append(extract_data_from_interactive_chart(url, content))
+
+    with open('Yahoo_Finance.txt', 'r') as f:
+        elements = [line.rstrip('\n').split(',') for line in f]
+    for description, symbol in elements:
+        frames.append(extract_data_from_yahoo_finance(description, symbol))
+
+    database = pd.concat(frames, axis=1, join='inner')
+    # database.to_csv('D:\Documentos\Master Big Data & Business Analytics\TFM\Códigos de prueba\Extracción y lectura de datos\Datos.csv', index_label='Date')
+
+
+if __name__ == "__main__":
+    main()
+
